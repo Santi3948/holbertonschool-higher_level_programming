ed24198172cae71bffb6f33670456da72dd13f6c
Santi3948 <santiagonegreyra@gmail.com>
2021-12-20 15:01:23 -0800

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,13 +2,16 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *aux = list;
-
+	
+	if(aux)
+	{
 	while(aux->next)
 	{
 		if(aux->next != list)
 			aux = aux->next;
 		else
 			return(1);
+	}
 	}
 	return (0);
 }
