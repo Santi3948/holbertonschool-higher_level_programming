a12c6840d06df2a391d5d147f7e4b689facef03a
Santi3948 <santiagonegreyra@gmail.com>
2021-12-21 17:29:05 -0800

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,48 +1,38 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
-  * insert_node - inserts a number into a sorted singly linked list
-  * @head: pointer to singly linked list
-  * @number: number to be inserted
-  * Return: pointer to node inserted, NULL if it fails
-  */
+ * insert_node - insert a node
+ * @number: the number
+ * @head: the list
+ * Return: the new node
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node = NULL, *temp = *head;
+	listint_t *aux = *head;
+	listint_t *node = malloc(sizeof(listint_t));
 
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	if (!node)
 		return (NULL);
-	new_node->n = number;
-	new_node->next = NULL;
-	if (!*head)
+	node->n = number;
+	node->next = NULL;
+	if (*head)
 	{
-		*head = new_node, (*head)->next = NULL;
-		return (new_node);
-	}
-	if ((*head)->next == NULL)
-	{
-		if ((*head)->n < new_node->n)
-			(*head)->next = new_node;
-		else
-			new_node->next = *head, *head = new_node;
-		return (new_node);
-	}
-	while (temp->next != NULL)
-	{
-		if (new_node->n < temp->n)
+		if ((*head)->n > number)
 		{
-			new_node->next = temp, *head = new_node;
-			return (new_node);
+			node->next = (*head);
+			*head = node;
+				return (node);
 		}
-		if (((new_node->n > temp->n) &&
-					(new_node->n < (temp->next)->n)) ||
-				(new_node->n == temp->n))
+
+		while ((*head)->next && (*head)->next->n < number)
 		{
-			new_node->next = temp->next, temp->next = new_node;
-			return (new_node);
+			*head = (*head)->next;
 		}
-		temp = temp->next;
+		node->next = (*head)->next;
+		(*head)->next = node;
+		*head = aux;
+		return (node);
 	}
-	temp->next = new_node;
-	return (new_node);
+		*head = node;
+		return (node);
 }
