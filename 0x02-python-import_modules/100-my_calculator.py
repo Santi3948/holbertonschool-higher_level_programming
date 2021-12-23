a2e697ab12bbf88770ccf43ce6fa55a1d367c0d2
Santi3948 <santiagonegreyra@gmail.com>
2021-12-23 11:51:40 -0800

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,18 +5,16 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] != "+" or "-" or "*" or "/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif argv[2] == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif argv[2] == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif argv[2] == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
-        a = int(argv[1])
-        b = int(argv[3])
-        if argv[2] == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif argv[2] == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif argv[2] == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        else:
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
