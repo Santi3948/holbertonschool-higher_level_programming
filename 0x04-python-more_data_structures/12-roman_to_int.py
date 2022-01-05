08631cebcbf15e24ba6238b3e9bee78625d1a69c
Santi3948 <santiagonegreyra@gmail.com>
2022-01-05 14:44:54 -0800

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import copy
-
-
 def roman_to_int(roman_string):
     roman = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     x = 0
@@ -11,8 +8,6 @@ def roman_to_int(roman_string):
     xd = [0]
     xd.extend(aux)
     aux.append(0)
-    print(*aux)
-    print(*xd)
     for j, w in zip(reversed(xd), reversed(aux)):
         if j != 0:
             if j >= w:
