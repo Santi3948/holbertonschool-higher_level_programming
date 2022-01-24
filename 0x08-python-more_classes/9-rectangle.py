56cb5b0fc959dd300ca13464810ac6e97339c92f
Santi3948 <santiagonegreyra@gmail.com>
2022-01-24 14:43:21 -0800

    puede ser

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,6 +9,16 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """constructor method"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if int(width) < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if int(height) < 0:
+            raise ValueError("height must be >= 0")
+        self.height = height
+        self.width = width
         self.height = height
         self.width = width
         type(self).number_of_instances += 1
