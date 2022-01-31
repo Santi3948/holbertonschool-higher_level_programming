7e4c15c7160edf0e09854bf1bf071605fe605b4c
Santi3948 <santiagonegreyra@gmail.com>
2022-01-31 13:03:38 -0800

    work locally

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class MyList(list):
+    """MyList class"""
+
+    def print_sorted(self):
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer" .format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0" .format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle"""
+
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        string = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return string
+
+
+class Square(Rectangle):
+	"""class Square"""
+
+    def __init__(self, size):
+        super().integer_validator(self, "size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
