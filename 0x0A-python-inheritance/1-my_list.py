2155d992ed3b51b39d29bef5ddcfded376aa1504
Santi3948 <santiagonegreyra@gmail.com>
2022-01-31 13:48:43 -0800

    work locally

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,4 +8,7 @@
 class MyList(list):
     """print sorted"""
     def print_sorted(self):
+        for item in self:
+            if type(item) != int:
+                raise TypeError("must be a list of integers")
         print(sorted(self))
