ec31cd586def5839d0894eeefd1db19ecf061b81
Santi3948 <santiagonegreyra@gmail.com>
2022-01-31 14:15:53 -0800

    work locally

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,50 +1,13 @@
 #!/usr/bin/python3
 """10. Square #1
-
-
 """
 
 
-class BaseGeometry:
-    """BaseGeometry class
-
-
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer" .format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0" .format(name))
-
-
-class Rectangle(BaseGeometry):
-    """class Rectangle
-
-
-    """
-
-    def __init__(self, width, height):
-        BaseGeometry.integer_validator("width", width)
-        BaseGeometry.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        string = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
-        return string
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """class Square
-
-
     """
 
     def __init__(self, size):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,30 +1,13 @@
 #!/usr/bin/python3
 """9. Full rectangle
-
-
 """
 
 
-class BaseGeometry:
-    """BaseGeometry class
-
-
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer" .format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0" .format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """class Rectangle
-
-
     """
 
     def __init__(self, width, height):
