f3a920a65d85182153a48b0532827e7b5db493ee
Tukson <91083046+Santi3948@users.noreply.github.com>
2022-02-01 23:07:20 -0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,9 +12,9 @@ class Student:
         """
         Instantiation with first_name, last_name and age
         """
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """
