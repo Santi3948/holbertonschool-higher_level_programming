09728091c0b1da6c746484a37c5f96be408234a6
Tukson <santiagonegreyra@gmail.com>
2022-02-07 08:27:44 -0800

    work locally

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,37 +25,6 @@ class Rectangle(Base):
     """
     class Rectangle
     """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Class constructor
-        """
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        else:
-            if width <= 0:
-                raise ValueError("width must be > 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        else:
-            if height <= 0:
-                raise ValueError("height must be > 0")
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        else:
-            if x < 0:
-                raise ValueError("x must be >= 0")
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        else:
-            if y < 0:
-                raise ValueError("y must be >= 0")
-
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        super().__init__(id)
-
     @property
     def width(self):
         return self.__width
@@ -104,6 +73,14 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+	 def __init__(self, width, height, x=0, y=0, id=None):
+        """constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     def area(self):
         """area doc"""
