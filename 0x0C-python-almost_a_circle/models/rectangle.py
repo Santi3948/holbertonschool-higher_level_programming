86348b59c3ea6248a59f1243b938dfa60572040a
Tukson <santiagonegreyra@gmail.com>
2022-02-07 08:10:38 -0800

    work locally

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python3
+"""
+2. First Rectangle
+"""
+
+
+class Base:
+    """
+    Class Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        class constructor
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+class Rectangle(Base):
+    """
+    class Rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Class constructor
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        else:
+            if width <= 0:
+                raise ValueError("width must be > 0")
+            else:
+                self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        else:
+            if height <= 0:
+                raise ValueError("height must be > 0")
+            else:
+                self.__height = height
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        else:
+            if x < 0:
+                raise ValueError("x must be >= 0")
+            else:
+                self.__x = x
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        else:
+            if y < 0:
+                raise ValueError("y must be >= 0")
+            else:
+                self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        """y setter"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    def area(self):
+        """area doc"""
+        return self.__height * self.__width
+
+    def display(self):
+        for w in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for z in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} \
+- {self.__width}/{self.__height}"
+
+    def update(self, *args, **kwargs):
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    setattr(self, "id", args[i])
+                if i == 1:
+                    setattr(self, "width", args[i])
+                if i == 2:
+                    setattr(self, "height", args[i])
+                if i == 3:
+                    setattr(self, "x", args[i])
+                if i == 4:
+                    setattr(self, "y", args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    setattr(self, "id", value)
+                if key == "width":
+                    setattr(self, "width", value)
+                if key == "height":
+                    setattr(self, "height", value)
+                if key == "x":
+                    setattr(self, "x", value)
+                if key == "y":
+                    setattr(self, "y", value)
