1915d152187be3afe7caee174f8a05a5329d3e49
Tukson <santiagonegreyra@gmail.com>
2022-04-04 13:24:38 -0700

    fixing 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ list.sort();
 const Max = Math.max(list);
 if (list.length >= 2) {
   for (let i = list.length - 1; i > 0; i--) {
-    if (Max != list[i]) {
+    if (Max !== list[i]) {
       console.log(list[i]);
       break;
     }
