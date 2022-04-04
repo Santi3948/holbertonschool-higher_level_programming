9607c356eda4a644da7af297eb8d13cc27fde548
Tukson <santiagonegreyra@gmail.com>
2022-04-04 12:39:42 -0700

    fixing 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < 3; i++) {
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing', null];
+for (let i = 0; list[i]; i++) {
   console.log(list[i]);
 }
