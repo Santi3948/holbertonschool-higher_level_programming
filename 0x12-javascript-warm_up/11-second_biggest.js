ea6d26d9c311dadce66e8ca94866362794efa4d7
Tukson <santiagonegreyra@gmail.com>
2022-04-04 14:33:02 -0700

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 const list = process.argv.slice();
 list.shift();
 list.shift();
-list.sort(function(a, b){return a-b});
-if(list.length === 0 || list.length === 1){
-	console.log(0);
-} else if(list[list.length - 2]){
-	console.log(list[list.length - 2]);
+list.sort(function (a, b) { return a - b; });
+if (list.length === 0 || list.length === 1) {
+  console.log(0);
+} else if (list[list.length - 2]) {
+  console.log(list[list.length - 2]);
 }
