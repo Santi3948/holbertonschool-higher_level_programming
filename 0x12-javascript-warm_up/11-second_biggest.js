fdc563413cff41bb496fe3ae3107e3192e7ee15a
Tukson <santiagonegreyra@gmail.com>
2022-04-04 14:30:52 -0700

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-const list = process.argv;
+const list = process.argv.slice();
 list.shift();
 list.shift();
-list.sort();
-const Max = Math.max(list);
-if (list.length >= 2) {
-  for (let i = list.length - 1; i > 0; i--) {
-    if (Max !== list[i]) {
-      console.log(list[i]);
-      break;
-    }
-  }
-} else {
-  console.log('0');
+list.sort(function(a, b){return a-b});
+if(list.length === 0 || list.length === 1){
+	console.log(0);
+} else if(list[list.length - 2]){
+	console.log(list[list.length - 2]);
 }
