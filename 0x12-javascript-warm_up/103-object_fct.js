daf95dddd111161570483da90465ddd48554395f
Tukson <santiagonegreyra@gmail.com>
2022-04-04 17:49:54 -0700

    fix 103!

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr () {
+myObject.incr = function (myObject) {
   myObject.value += 1;
-}
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
