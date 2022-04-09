517b606a3522bfc6b491af478bca9f7eb19b5b6d
Santi3948 <santiagonegreyra@gmail.com>
2022-04-09 14:11:17 -0700

    fix 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module for print states"""
+"""Module for print states
+"""
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
