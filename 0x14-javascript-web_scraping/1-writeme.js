68e9932dda648c824ad751a7a1de6fa2144b7c0f
Tukson <santiagonegreyra@gmail.com>
2022-05-17 15:57:54 -0700

    fixing 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const args = process.argv;
 const content = args[3];
-fs.writeFile(args[2], 'utf8', content, err => {
+fs.writeFile(args[2], content, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   }
